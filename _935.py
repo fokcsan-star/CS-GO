0b5e37ff9a1ce0746f030e40665a9e4fd6bcff81
fokcsan-star <fokcsan@gmail.com>
2025-12-01 14:05:41 +0700

    Add files via upload

--- _935.py
+++ _935.py
@@ -11,14 +11,30 @@ class Vehicle(ABC):
 
 class Car(Vehicle):
     def start_engine(self):
+        print("Car: Starting engine... Vroom!")
         return "Car engine started"
     
     def stop_engine(self):
+        print("Car: Stopping engine...")
         return "Car engine stopped"
 
 class Bike(Vehicle):
     def start_engine(self):
+        print("Bike: Starting engine... Brrrr!")
         return "Bike engine started"
     
     def stop_engine(self):
+        print("Bike: Stopping engine...")
         return "Bike engine stopped"
+
+
+
+car = Car()
+car.start_engine()
+car.stop_engine()
+print()
+
+bike = Bike()
+bike.start_engine()
+bike.stop_engine()
+print()
