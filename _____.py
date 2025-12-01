b9ab33ac52d245a9b40fdfcf86c0120093429a18
fokcsan-star <fokcsan@gmail.com>
2025-12-01 13:41:26 +0700

    Add files via upload

--- _____.py
+++ _____.py
@@ -1,20 +1,10 @@
-import math
-
-class Shape:
-    def area(self): return 0
-    def perimeter(self): return 0
-
-class Circle(Shape):
-    def __init__(self, r): self.r = r
-    def area(self): return math.pi * self.r ** 2
-    def perimeter(self): return 2 * math.pi * self.r
-
-class Triangle(Shape):
-    def __init__(self, a, b, c): 
-        self.a = a
-        self.b = b
-        self.c = c
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+    
     def area(self):
-        p = self.perimeter() / 2
-        return math.sqrt(p * (p - self.a) * (p - self.b) * (p - self.c))
-    def perimeter(self): return self.a + self.b + self.c
+        return self.width * self.height
+    
+    def perimeter(self):
+        return 2 * (self.width + self.height)
