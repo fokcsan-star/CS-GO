8156fb56a2c051e4f256977a935b62540ee9a410
fokcsan-star <fokcsan@gmail.com>
2025-12-01 13:54:01 +0700

    Add files via upload

---
+++ _935.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+
+class Vehicle(ABC):
+    @abstractmethod
+    def start_engine(self):
+        pass
+    
+    @abstractmethod
+    def stop_engine(self):
+        pass
+
+class Car(Vehicle):
+    def start_engine(self):
+        return "Car engine started"
+    
+    def stop_engine(self):
+        return "Car engine stopped"
+
+class Bike(Vehicle):
+    def start_engine(self):
+        return "Bike engine started"
+    
+    def stop_engine(self):
+        return "Bike engine stopped"
